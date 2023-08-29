a0681aede879261b4656c8c6f3d57e646de9f615
over <franciscosebastianyjunior@hotmail.com>
2023-08-28 22:04:01 -0300

    Fix player's speed being 0

--- src/systems/setup.rs
+++ src/systems/setup.rs
@@ -1,10 +1,14 @@
 use bevy::prelude::*;
-use crate::bundles::PlayerCharacter;
+use crate::{bundles::PlayerCharacter, components::Movement};
 
 pub fn setup(mut cmds: Commands, asset_server: Res<AssetServer>) {
    cmds.spawn(Camera2dBundle::default());
    
    cmds.spawn(PlayerCharacter {
+        movement: Movement {
+            speed: 300.,
+            ..default()
+        },
         sprite_bundle: SpriteBundle {
             texture: asset_server.load("test-arcanist.png"),
             ..default()
