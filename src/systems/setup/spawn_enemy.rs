52fb06471e832a4ccaa9fb749989dbd023ddca34
over <franciscosebastianyjunior@hotmail.com>
2023-09-01 17:28:41 -0300

    Nerf enemy speed and make it spawn off-screen

--- src/systems/setup/spawn_enemy.rs
+++ src/systems/setup/spawn_enemy.rs
@@ -4,13 +4,13 @@ use crate::{bundles::EnemyCharacter, components::Movement};
 pub fn spawn_enemy(mut cmds: Commands, asset_server: Res<AssetServer>) {
     cmds.spawn(EnemyCharacter {
         movement: Movement {
-            speed: 250.,
+            speed: 125.,
             ..default()
         },
         sprite_bundle: SpriteBundle {
             texture: asset_server.load("test-arcanist.png"),
             transform: Transform {
-                translation: Vec3::new(0., 200., 0.),
+                translation: Vec3::new(0., 400., 0.),
                 ..default()
             },
             ..default()
